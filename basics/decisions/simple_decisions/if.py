945db563a0248f040891440f38db87106ed79ad7
aler1999 <>
2021-10-28 11:47:21 +0000

    Added code to demonstrate the use of a modulus operator.

--- basics/decisions/simple_decisions/if.py
+++ basics/decisions/simple_decisions/if.py
@@ -1,8 +1,10 @@
+
 #Asking user what kind of book
 print("What type of book is this?")
 book_type = input()
 
-#Print book type
-print("\nI like {} books!".format(book_type))
+#Telling the user Beep likes adventure books if user inserted advenuture books as kind of book
+if book_type == "adventure" :
+  print("\nI like adventure books!")
 
 print("\nFinished reading book.")
---
+++ basics/decisions/simple_decisions/if_elif_else.py
@@ -0,0 +1,12 @@
+direction = input("Towards which direction should I paint (up, down, left or right)?\n")
+
+if direction == "up" :
+  print ("I am painting in the upward direction!")
+elif direction == "down" :
+  print ("I am painting in the downward direction!")
+elif direction == "left" :
+  print ("I am painting in the leftward direction!")
+elif direction == "right" :
+  print ("I am painting in the rightward direction!")
+else:
+    print("I do not know this command, tell me a direction!")
