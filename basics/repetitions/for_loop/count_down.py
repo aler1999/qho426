1d435e27185e40b4d88872f8a0ec78cae4767669
aler1999 <>
2021-11-10 10:53:59 +0000

    Added code to demonstrate counting down with a for loop.

---
+++ basics/repetitions/for_loop/count_down.py
@@ -0,0 +1,4 @@
+hf = int(input("How far are we from the cave? "))
+
+for i in range(hf, 0, -1):
+  print(f"{i} steps remaining")
---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,11 @@
+n = int(input("How many mountains should I display? "))
+
+print("\nDisplaying...\n")
+
+for i in range(0, n):
+  print("           __")
+  print("          /  \_")
+  print("         /^    \ ")
+  print("        /  ^    \_")
+  print("      _/ ^ ^     ^\ ")
+  print("     /  ^     ^    \ ")
